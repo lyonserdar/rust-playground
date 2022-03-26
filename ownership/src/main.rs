4fe4ee2719afbf4866d2080216bff3760830a4e3
Serdar Lyon <lyonserdar@gmail.com>
2022-03-26 13:29:54 -0600

    ownership and references in rust

---
+++ ownership/src/main.rs
@@ -0,0 +1,45 @@
+#![allow(unused_mut, unused_variables)]
+
+fn main() {
+    let mut arg: String = std::env::args().nth(1).unwrap_or_else(|| {
+        println!("Please supply an argument to this program.");
+        std::process::exit(-1);
+    });
+
+    inspect(&arg);
+
+    change(&mut arg);
+    println!("I have many {}", arg);
+
+    inspect(&arg);
+
+    if eat(arg) {
+        println!("Might be bananas");
+    } else {
+        println!("Not bananas");
+    }
+
+    println!("1 + 2 = {}, even via references", add(&1, &2));
+}
+
+fn inspect(s: &String) {
+    if s.ends_with("s") {
+        println!("{} is plural", s);
+    } else {
+        println!("{} is singular", s);
+    }
+}
+
+fn change(s: &mut String) {
+    if !s.ends_with("s") {
+        s.push_str("s");
+    }
+}
+
+fn eat(s: String) -> bool {
+    s.starts_with("b") && s.contains("a")
+}
+
+fn add(x: &i32, y: &i32) -> i32 {
+    x + y
+}
