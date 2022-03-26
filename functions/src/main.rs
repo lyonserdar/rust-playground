f6554cb5e2111c6510085a029e5af60f66b5c8b2
Serdar Lyon <lyonserdar@gmail.com>
2022-03-25 21:54:01 -0600

    functions in rust

---
+++ functions/src/main.rs
@@ -0,0 +1,21 @@
+#![allow(unused_variables)]
+
+fn main() {
+    let width = 4;
+    let height = 7;
+    let depth = 10;
+
+    let area = area_of(width, height);
+    println!("Area is {}", area);
+
+    println!("Volume is {}", volume(width, height, depth));
+
+}
+
+fn volume(width: i32, height: i32, depth: i32) -> i32 {
+    width * height * depth
+}
+
+fn area_of(x: i32, y:i32) -> i32 {
+    x * y
+}
